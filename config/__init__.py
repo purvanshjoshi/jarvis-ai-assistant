1756180a4fe9c7eec1cdb4b1edb3cbc2a213ecc1
Purvansh Joshi <purvanshjoshi7534011576@gmail.com>
2025-12-01 14:46:37 +0530

    Add config package initialization file

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+"""
+Configuration module for J.A.R.V.I.S system.
+
+This module initializes the configuration settings for the application.
+"""
+
+from .settings import Config, config
+
+__all__ = ['Config', 'config']
